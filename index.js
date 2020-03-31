d573cbdefc250fd2ac86430efc982c6f0a88ec57
Andrei Lizardi <andrei.lizardi@gmail.com>
2020-03-31 16:21:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,19 +2,3 @@
 var name = "Susan"
 var height = 74
 var message = `${name} is ${height} inches tall`
-
-module.exports = {name, height, message}
-console.log(message)
-
- describe('name', () => {
-    it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
-    })
-  })
-  
-    describe("Message", () => {
-    it("gives the name and height", () => {
-      expect(index.message).toInclude(index.name)
-      expect(index.message).toInclude(index.height)
-    })
-  })
