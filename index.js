ae7c790c71f8197e5c963d9421112939cf1080b2
Andrei Lizardi <andrei.lizardi@gmail.com>
2020-03-31 16:20:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var message = `${name} is ${height} inches tall`
 module.exports = {name, height, message}
 console.log(message)
 
- describe('Name', () => {
+ describe('name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
     })
